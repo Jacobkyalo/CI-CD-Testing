7500eb9a334ae030cde3b42c8d06fc017b1b890d
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-06-17 09:55:32 +0300

    error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import "./App.css";
 
 function App() {
-  const printName = (name) => {
-    console.log(name);
-  };
+  // const printName = (name) => {
+  //   console.log(name);
+  // };
 
   return (
     <>
